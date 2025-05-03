ece96036d7ba621892ce9f2db3ba5f8c7e4ff3b7
Michail19 <Mi.ershov.2017@yandex.ru>
2025-05-03 18:35:37 +0300

    Small changes in route

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,12 +1,12 @@
 import { ProfileUI } from '@ui-pages';
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { selectUserData, updateUser } from '../../slices/user-slice';
 
 export const Profile: FC = () => {
   /** TODO: взять переменную из стора */
-  const user = {
-    name: '',
-    email: ''
-  };
+  const user = useSelector(selectUserData);
+  const dispatch = useDispatch();
 
   const [formValue, setFormValue] = useState({
     name: user.name,
@@ -29,6 +29,7 @@ export const Profile: FC = () => {
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(updateUser(formValue));
   };
 
   const handleCancel = (e: SyntheticEvent) => {
@@ -57,5 +58,5 @@ export const Profile: FC = () => {
     />
   );
 
-  return null;
+  // return null;
 };
