9573188306ed46953e025a34198b522aa8e1c059
Michail19 <Mi.ershov.2017@yandex.ru>
2025-05-05 10:21:20 +0300

    Login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,19 +1,31 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent, useState, useEffect } from 'react';
 import { LoginUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
-import { loginUser } from '../../slices/user-slice';
-import { selectLoginError } from '../../slices/user-slice';
+import {
+  loginUser,
+  selectLoginError,
+  selectIsAuthenticated
+} from '../../slices/user-slice';
+import { useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const error = useSelector(selectLoginError);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const navigate = useNavigate();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
     dispatch(loginUser({ email, password }));
   };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/'); // Перенаправление на главную страницу
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <LoginUI
