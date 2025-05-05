470ec50b63955e9e8f872a48ad3347092c2cd974
Michail19 <Mi.ershov.2017@yandex.ru>
2025-05-05 14:54:11 +0300

    Test preparing

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@ui/(.*)$': '<rootDir>/src/components/ui/$1',
+    '^@ui-pages/(.*)$': '<rootDir>/src/components/ui/pages/$1',
+    '^@utils-types/(.*)$': '<rootDir>/src/utils/types/$1',
+    '^@api$': '<rootDir>/src/utils/burger-api.ts',
+    '^@slices/(.*)$': '<rootDir>/src/services/slices/$1',
+    '^@selectors/(.*)$': '<rootDir>/src/services/selectors/$1',
+
+    '\\.(css|scss|module\\.css)$': 'identity-obj-proxy',
+
+    '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/*.test.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json'
+    }
+  }
+};
